151116d131fa9a21bd9728b3587acb3c83e10dd1
Nikita <jumper211212@gmail.com>
2018-02-14 15:46:43 +0200

    this is works too

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var user = require('./routes/user');
+var HttpError = require('./error/index').HttpError;
+var errorHandler = require('express-error-handler');
+var config = require('./config');
+var mongoose = require('./lib/mongoose');
+var session = require('express-session')
 
 var app = express();
 
@@ -22,28 +23,63 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+var MongoStore = require('connect-mongo')(session);
+app.use(session({
+  secret: config.get('session:secret'),
+  key: config.get('session:key'),
+  store: new MongoStore({mongooseConnection: mongoose.connection })
+})); //connect.sid - cookie for user when it came first
+
+app.use(function(req, res, next){
+  req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+  res.send("Visits: " + req.session.numberOfVisits);
+});
+
+app.use(require('./middleware/sendHttpError'));
+// app.use(app.router);
+require('./routes/index')(app);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/user', user);
+// app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+app.use(function(err, req, res, next) {
+  if (typeof err == 'number') { // next(404);
+    err = new HttpError(err);
+  }
+
+  if (err instanceof HttpError) {
+    res.sendHttpError(err);
+  } else {
+    if (app.get('env') == 'development') {
+      errorHandler()(err, req, res, next);
+    } else {
+      log.error(err);
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }
+  }
 });
+
+
+
+
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,54 @@
-var express = require('express');
-var router = express.Router();
+var User = require('../models/user').User;
+var HttpError = require('../error/index').HttpError;
+var ObjectID = require('mongodb').ObjectID;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    body: 'SUKA'
+module.exports = function(app){
+
+  app.get('/', function(req, res, next){
+    res.render('index');
   });
-});
 
-module.exports = router;
+  app.get('/users', function(req, res, next) {
+    User.find({}, function(err, users){
+      if (err) return next(err);
+      res.json(users);
+    });
+  });
+
+  app.get('/user/:id', function(req, res, next) {
+    try {
+      var id = new ObjectID(req.params.id);
+    } catch (e) {
+      return next(404);
+    }
+
+    User.findById(id, function(err, user){
+      if (err) {
+        return next(err);
+      }
+      else {
+        if(!user) {
+          next(new HttpError(404, 'User not found'));
+        }
+        else {
+          res.json(user);
+        }
+      }
+    });
+  });
+
+};
+
+
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', {
+//     title: 'Express',
+//     body: 'SUKA'
+//   });
+// });
+//
+// module.exports = router;
